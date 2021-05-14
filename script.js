2d9c05e4a626f3412de0af6bbce85a9b6d636417
michaellamoon <78895904+michaellamoon@users.noreply.github.com>
2021-05-14 16:43:31 -0700

    edit

--- script.js
+++ script.js
@@ -21,3 +21,32 @@ el4.addEventListener("mousemove", (e) => {
   el4.style.backgroundPositionX = -e.offsetX + "px";
   el4.style.backgroundPositionY = -e.offsetY + "px";
 });
+
+const el5 = document.querySelector("#module5");
+el5.addEventListener("mousemove", (e) => {
+  el5.style.backgroundPositionX = -e.offsetX + "px";
+  el5.style.backgroundPositionY = -e.offsetY + "px";
+});
+
+const el6 = document.querySelector("#module6");
+el6.addEventListener("mousemove", (e) => {
+  el6.style.backgroundPositionX = -e.offsetX + "px";
+  el6.style.backgroundPositionY = -e.offsetY + "px";
+});
+
+const el7 = document.querySelector("#module7");
+el7.addEventListener("mousemove", (e) => {
+  el7.style.backgroundPositionX = -e.offsetX + "px";
+  el7.style.backgroundPositionY = -e.offsetY + "px";
+});
+
+const el8 = document.querySelector("#module8");
+el8.addEventListener("mousemove", (e) => {
+  el8.style.backgroundPositionX = -e.offsetX + "px";
+  el8.style.backgroundPositionY = -e.offsetY + "px";
+});
+const el9 = document.querySelector("#module9");
+el9.addEventListener("mousemove", (e) => {
+  el9.style.backgroundPositionX = -e.offsetX + "px";
+  el9.style.backgroundPositionY = -e.offsetY + "px";
+});
